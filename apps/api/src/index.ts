4081ddcbb9e225b0854d5ce5b01f240be3d41491
Emanuel Bacalhau <carlosemanuelbatistabacalhau@gmail.com>
2025-02-02 01:19:23 -0300

    feat: add setup for database and install libraries

--- apps/api/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { defineAbilityFor } from '@saas/auth'
-
-const ability = defineAbilityFor({
-  role: 'ADMIN',
-})
-
-console.log(ability.can('manage', 'all')) // true;
-console.log(ability.can('manage', 'User')) // true;
-console.log(ability.cannot('manage', 'User')) // true;
