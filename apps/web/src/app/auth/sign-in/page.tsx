5281555618c58ba863037ff58c293b9fd1c24cae
Emanuel Bacalhau <carlosemanuelbatistabacalhau@gmail.com>
2025-02-04 13:22:24 -0300

    feat: add action for sign in

--- apps/web/src/app/auth/sign-in/page.tsx
+++ apps/web/src/app/auth/sign-in/page.tsx
@@ -1,13 +1,16 @@
+'use client'
+
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Separator } from '@/components/ui/separator'
 import { IconBrandGithub } from '@tabler/icons-react'
 import Link from 'next/link'
+import { signInWithEmailAndPassword } from './actions'
 
 export default function SignInPage() {
   return (
-    <form action="" className="space-y-4">
+    <form action={signInWithEmailAndPassword} className="space-y-4">
       <div className="space-y-1">
         <Label htmlFor="email">E-mail</Label>
         <Input type="email" name="email" id="email" />
