a9495550001930a8128966e920494fedf2477c24
Emanuel Bacalhau <carlosemanuelbatistabacalhau@gmail.com>
2025-02-03 23:57:25 -0300

    feat: add flow for create invite

---
+++ apps/api/src/http/routes/invite/create-invite.ts
@@ -0,0 +1,116 @@
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+import { responseSwaggerSchema } from '@/lib/response-swagger-schema'
+import { getUserPermission } from '@/utils/get-user-permission'
+import { roleSchema } from '@saas/auth'
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+import { BadRequestError } from '../_errors/bad-request-error'
+
+export async function createInvite(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .post(
+      '/organizations/:slug/invites',
+      {
+        schema: {
+          tags: ['invites'],
+          summary: 'Create a new invite',
+          security: [{ bearerAuth: [] }],
+          body: z.object({
+            email: z.string().email(),
+            role: roleSchema,
+          }),
+          params: z.object({
+            slug: z.string().nonempty(),
+          }),
+          response: responseSwaggerSchema([
+            {
+              code: 201,
+              schema: z.object({
+                inviteId: z.string(),
+              }),
+            },
+            {
+              code: 400,
+            },
+            {
+              code: 401,
+            },
+            {
+              code: 500,
+            },
+          ]),
+        },
+      },
+      async (request, reply) => {
+        const userId = await request.getCurrentUserId()
+        const { slug } = request.params
+
+        const { membership, organization } =
+          await request.getUserMembership(slug)
+
+        const ability = getUserPermission(userId, membership.role)
+
+        if (ability.cannot('create', 'Invite')) {
+          throw new BadRequestError('You do not allowed to create new invite')
+        }
+
+        const { email, role } = request.body
+
+        const [, domain] = email.split('@')
+
+        if (
+          organization.shouldAttachUsersByDomain &&
+          organization.domain === domain
+        ) {
+          throw new BadRequestError(
+            `Users with ${domain} domain will join your organization automatically on login`
+          )
+        }
+
+        const inviteWithSameEmail = await prisma.invite.findUnique({
+          where: {
+            email_organizationId: {
+              email,
+              organizationId: organization.id,
+            },
+          },
+        })
+
+        if (inviteWithSameEmail) {
+          throw new BadRequestError(
+            'Another invite with this email already exists'
+          )
+        }
+
+        const membershipWithSameEmail = await prisma.member.findFirst({
+          where: {
+            organizationId: organization.id,
+            user: {
+              email,
+            },
+          },
+        })
+
+        if (membershipWithSameEmail) {
+          throw new BadRequestError('A member with this email already exists')
+        }
+
+        const invite = await prisma.invite.create({
+          data: {
+            email,
+            role,
+            organizationId: organization.id,
+            authorId: userId,
+          },
+        })
+
+        return reply.code(201).send({
+          inviteId: invite.id,
+        })
+      }
+    )
+}
--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -17,6 +17,7 @@ import { createAccount } from './routes/auth/create-account'
 import { getProfile } from './routes/auth/get-profile'
 import { requestPasswordRecover } from './routes/auth/request-password-recover'
 import { resetPassword } from './routes/auth/reset-password'
+import { createInvite } from './routes/invite/create-invite'
 import { getMembers } from './routes/member/get-members'
 import { removeMember } from './routes/member/remove-member'
 import { updateMember } from './routes/member/udpate-member'
@@ -94,6 +95,8 @@ app.register(updateProject)
 app.register(getMembers)
 app.register(updateMember)
 app.register(removeMember)
+
+app.register(createInvite)
 
 app
   .listen({
