f441c0a7e74cc79090b5efb941f8ff09510f9b24
Emanuel Bacalhau <carlosemanuelbatistabacalhau@gmail.com>
2025-02-05 01:09:29 -0300

    feat: add middleware for get current organization and organization switcher

---
+++ apps/web/src/app/(app)/org/[slug]/page.tsx
@@ -0,0 +1,13 @@
+import { Header } from '@/components/header'
+
+export default async function Projects() {
+  return (
+    <div className="py-4">
+      <Header />
+
+      <main>
+        <h1>Project</h1>
+      </main>
+    </div>
+  )
+}
---
+++ apps/web/src/components/organization-switcher.tsx
@@ -0,0 +1,74 @@
+import { getOrganizations } from '@/http/get-organization'
+import { ChevronsUpDown, PlusCircleIcon } from 'lucide-react'
+import { cookies } from 'next/headers'
+import Link from 'next/link'
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu'
+
+export async function OrganizationSwitcher() {
+  const cookiesStore = await cookies()
+  const { organizations } = await getOrganizations()
+  const currentOrg = cookiesStore.get('org')?.value
+
+  const currentOrganization = organizations.find(org => org.slug === currentOrg)
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="flex w-[168px] items-center gap-2 rounded p-1 text-sm font-medium outline-none focus-visible:ring-2 focus-visible:ring-primary">
+        {currentOrganization ? (
+          <>
+            <Avatar className="size-5 mr-1">
+              <AvatarImage src={currentOrganization.avatarUrl} />
+              <AvatarFallback />
+            </Avatar>
+            <span className="truncate text-left">
+              {currentOrganization.name}
+            </span>
+          </>
+        ) : (
+          <span className=" text-muted-foreground">Select organization</span>
+        )}
+        <ChevronsUpDown className="size-4 ml-auto text-muted-foreground" />
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent
+        align="end"
+        alignOffset={-16}
+        sideOffset={12}
+        className="w-[200px]"
+      >
+        <DropdownMenuGroup>
+          <DropdownMenuLabel>Organizations</DropdownMenuLabel>
+          {organizations.map(organization => (
+            <DropdownMenuItem key={organization.id} asChild>
+              <Link href={`/org/${organization.slug}`}>
+                <Avatar className="size-5 mr-1">
+                  <AvatarImage src={organization.avatarUrl} />
+                  <AvatarFallback />
+                </Avatar>
+                <span className="truncate">{organization.name}</span>
+              </Link>
+            </DropdownMenuItem>
+          ))}
+        </DropdownMenuGroup>
+
+        <DropdownMenuSeparator />
+
+        <DropdownMenuItem asChild>
+          <Link href="/create-organization">
+            <PlusCircleIcon className="size-5 mr-1 text-primary" />
+            Create new
+          </Link>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
